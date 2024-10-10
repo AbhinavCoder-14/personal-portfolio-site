d2132b616f5074c43373abbba76ff011d39cef15
Abhinav yadav <kanikayadav061@gmail.com>
2024-10-10 17:20:35 +0530

    Responsive on small phones

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName('responsive-work').addEventListener('click', function() {
+    const container = document.getElementsByClassName('center-Profile-side');
+    container.classList.toggle('left-transform'); // Toggle the class to trigger the transformation
+});
